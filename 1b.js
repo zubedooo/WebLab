05e20193f7abc6ab623f3863b98a8a22cdbea941
Tushar Saxena <tushar.saxena250@gmail.com>
2019-05-24 02:32:40 +0530

    1b

---
+++ 1b.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+var mongoClient = require('mongodb').MongoClient;
+
+mongoClient.connect('mongodb://localhost', function(err, client) {
+    if(!err){
+        console.log("Connected...")
+        var db = client.db('students');
+        app.use(express.static('public'));
+        app.get('/1b.html', function(req, res) {
+            res.sendFile(__dirname+'/'+'1b.html');
+        });
+        app.get('/insert', function(req, res) {
+            db.collection('student').insert({usn:req.query.usn, name:req.query.name, sub_code:req.query.code, marks:parseInt(req.query.marks)});
+            res.end(JSON.stringify(req.query));
+        });
+        app.get('/display', function(req, res) {
+            db.collection('student').find().sort().toArray(function(err, data){
+                console.log(data);
+                res.end(JSON.stringify(data));
+            });
+        });
+        app.get('/result', function(req, res) {
+            db.collection('student').find({marks:{$lt:20}}).sort().toArray(function(err, data){
+                console.log(data);
+                res.end(JSON.stringify(data));
+            });
+        });
+        app.listen(3000);
+    }
+});
